aa929b5eb9090e82162b18e852e1ff28fc03bc87
eleavydev <elaineleavy@gmail.com>
2022-06-17 21:37:07 +0000

    Display addition question to the user

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,9 +11,12 @@ document.addEventListener("DOMContentLoaded", function() {
             } else {
                 let gameType = this.getAttribute("data-type");
                 alert(`You clicked ${gameType}`);
+                runGame(gameType);
             }
         })
      }
+
+     runGame("addition");
 })
 
 /**
@@ -21,12 +24,18 @@ document.addEventListener("DOMContentLoaded", function() {
  * and after the user's answer has been processed. 
  * Gives a quick reminder of what the function does
  */
-function runGame() {
+function runGame(gameType) {
     let num1 = Math.floor(Math.random() * 25) + 1;
     let num2 = Math.floor(Math.random() * 25) + 1;
+
+    if (gameType === "addition") {
+        displayAdditionQuestion(num1, num2);
+    }else {
+    alert(`Unknown game type: ${gameType}`);
+    throw `Unknown game type: ${gameType}. Aborting! `;
+    }
+
 }
-
-
 
 function checkAnswer() {
     
@@ -44,7 +53,11 @@ function incrementWrongAnswer() {
     
 }
 
-function displayAdditionQuestion() {
+function displayAdditionQuestion(operand1, operand2) {
+    
+    document.getElementById('operand1').textContent = operand1;
+    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operator').textContent = "+";
     
 }
 
